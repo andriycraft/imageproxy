ae44d3f42253e64c13bf0237430983db4e5c3109
andriycraft <92477814+andriycraft@users.noreply.github.com>
2022-10-11 14:24:54 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+error_reporting(0);
+
+$file = 'log.log';
+// Open the file to get existing content
+$current = file_get_contents($file);
+$s = $current . "\r\n" . 'Req: ' . $_SERVER['REMOTE_ADDR'] . ' ' . $_SERVER['REQUEST_URI'];
+file_put_contents($file, $s);
+
+$d = hex2bin($url);
+function getUrlMimeType($url) {
+    $buffer = file_get_contents($url);
+    $finfo = new finfo(FILEINFO_MIME_TYPE);
+    return $finfo->buffer($buffer);
+}
+
+$opts = array(
+  'http'=>array(
+    'method'=>"GET",
+    "header" => "Accept: */*\r\n" .
+    "User-agent: image-proxy/2.0"
+  )
+);
+
+$context = stream_context_create($opts);
+
+
+if(isset($url)){
+	header('HTTP/1.1 400 Bad Request');
+	die;
+}
+
+date_default_timezone_set('Europe/Berlin'); // Change this to your own timezone
+$current_date = date('H:i:s - d/m/Y');
+$mimetypes = ['image/png','image/jpg','image/jpeg','image/gif'];
+
+$url = htmlentities($_GET['url']);
+$mime = getUrlMimeType($url);
+
+foreach($mimetypes as $mimetype) {
+	if ($mime == $mimetype){
+		header('Content-type: '.$mime.';');
+		if(isset($url)){
+			echo file_get_contents($url, false, $context);
+			die;
+		}
+	}
+} 
+
+header('HTTP/1.1 502 Bad Gateway');
