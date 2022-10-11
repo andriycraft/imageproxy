dbafa6e4ae2190700d985e76d82017c7927a3438
andriycraft <92477814+andriycraft@users.noreply.github.com>
2022-10-11 14:39:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,18 +2,6 @@
 
 $url = htmlentities($_GET['url']);
 
-if(!$url){
-	header('HTTP/1.1 400 Bad Request');
-	die;
-}
-
-$d = hex2bin($url);
-function getUrlMimeType($url) {
-    $buffer = file_get_contents($url);
-    $finfo = new finfo(FILEINFO_MIME_TYPE);
-    return $finfo->buffer($buffer);
-}
-
 $opts = array(
   'http'=>array(
     'method'=>"GET",
@@ -24,11 +12,23 @@ $opts = array(
 
 $context = stream_context_create($opts);
 
+if(!$url){
+	header('HTTP/1.1 400 Bad Request');
+	die;
+}
+
+function getUrlMimeType($url) {
+    $buffer = file_get_contents($url);
+    $finfo = new finfo(FILEINFO_MIME_TYPE);
+    return $finfo->buffer($buffer);
+}
+
+
 date_default_timezone_set('Europe/Berlin'); // Change this to your own timezone
 $current_date = date('H:i:s - d/m/Y');
 $mimetypes = ['image/png','image/jpg','image/jpeg','image/gif'];
 
-$mime = getUrlMimeType($url);
+$mime = getUrlMimeType($url, false, $context);
 
 foreach($mimetypes as $mimetype) {
 	if ($mime == $mimetype){
